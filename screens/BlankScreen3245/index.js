12ae8f7b1d503fb0a0e48943d5a73ccf6965c207
Crowdbotics <engineering@crowdbotics.com>
2022-12-19 02:14:49 +0000

    Add Blank Screen 3245

---
+++ screens/BlankScreen3245/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen3245 = () => {
+  return <View style={_styles.BlankScreen3245}>
+      </View>;
+};
+
+export default BlankScreen3245;
+
+const _styles = StyleSheet.create({
+  BlankScreen3245: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
